e72c7ba4030a3f5ea9d898b374553ddb4ac53d6a
Andrew Gwinner <weblings.lings@gmail.com>
2026-09-15 19:57:05 -0700

    Endpoints connection work

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,7 @@
 #include <Aurora/Output/Hue/Credentials.hpp>
 #include <Aurora/Output/Hue/CredentialsStore.hpp>
 #include <Aurora/Output/Hue/HueOutput.hpp>
+#include <Aurora/Output/Hue/PairingRoutes.hpp>
 #endif
 
 namespace
@@ -213,6 +214,9 @@ try
 
   Aurora::Network::Http::Server::HttpServer httpServer;
   registerCapabilitiesRoute(httpServer, registry);
+#ifdef AURORA_OUTPUT_HUE_IO_AVAILABLE
+  Aurora::Output::Hue::registerPairingRoutes(httpServer, configRoot);
+#endif
 
   Aurora::Runtime::ConfigStore configStore(configRoot);
   Aurora::Runtime::Config config = configStore.load();
